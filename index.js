2a7e3bbca7e7222b69648096726679a1d2f0bc79
bnag9480-spec <bnag9480@gmail.com>
2026-04-18 17:56:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const Eris = require("eris");
 const keep_alive = require('./keep_alive.js')
 
-// Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+  console.error(err);
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.on("ready", () => {
+  console.log("Custom status is now active!");
+  
+  bot.editStatus("online", {
+    name: "Custom Status", 
+    type: 4,
+    state: ".gg/rollbet" 
+  });
+});
+
+bot.connect();
